65e605c0617db57ddcb9705822040463797c50b5
jungmyungjin <myung2667@gmail.com>
2020-03-17 00:11:24 +0900

    [nomadcoders]Make_web_service - first Component and JSX
    [ReactJS로 웹 서비스 만들기] 강의 실습 및 노트 : #2.0 ~ 2.1
    #2.0 Creating your first React Component
    #2.1 Reusable Components with JSX + Props

--- [nomadcoders]Make_web_service_with_ReactJS/movie_app/src/App.js
+++ [nomadcoders]Make_web_service_with_ReactJS/movie_app/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+function Food({ fav }) {
+  // function Food(props) {
+  return <h1> I'm {fav} </h1>;
+  // return <h1> I'm {props.fav} </h1>;
+}
+
 function App() {
-  return <div>Hello!!!!!!</div>;
+  return (
+    <div>
+      <h1>Hello!!!!</h1>
+      <Food fav="Pizza" />
+      <Food fav="Chicken" />
+      <Food fav="dorazi" />
+      <Food fav="goguma" />
+    </div>
+  );
 }
 
 export default App;
